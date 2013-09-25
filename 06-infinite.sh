a5ff3e2cc05d2fff594bab433159182960786d1e
Tacksoo Im <tacksoo.im@gmail.com>
2013-09-25 00:01:29 -0400

    infinite loop added

---
+++ 06-infinite.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# the if statement had, if-then-fi
+# while loop has, while-do-done
+# true and false are also Unix commands
+while [ true ]; do
+  echo "infinite number of beer on the wall"
+done
