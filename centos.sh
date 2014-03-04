f9597238add67a81059694f02f553046486d2faa
Tacksoo Im <tacksoo.im@gmail.com>
2014-03-04 13:07:15 -0500

    basic centos commands

---
+++ centos.sh
@@ -0,0 +1,14 @@
+# common commands for CentOS
+
+# list all repos
+yum repolist all
+
+# enable a repo
+yum-config-manager --enable elrepo-extras
+
+# check CentOS version
+cat /etc/centos-release
+
+# update packages
+yum update
+
