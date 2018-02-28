f0e1aec7fdbf3eeb527b6a8a6b1828792730685f
jnbmaster <jnb.cb38@gmail.com>
2018-02-28 12:46:04 -0500

    Modified 01-read_input.sh

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -10,3 +10,7 @@ echo 'Your name was stored in $name'
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
+echo "What would you like to name your file? Please include the extension."
+read name
+touch name
+
