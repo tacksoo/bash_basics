d7eec226faed87d481e6ddcc5fb3d8547caf2bf4
Wil <wnazario@ggc.edu>
2018-02-28 11:39:04 -0500

    added some very strange height and weight measurement for difference

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -11,5 +11,12 @@ sum=$(( first+second+third ))
 
 echo "The sum is $sum"
 
+echo "Enter your real height in person"
+read realHeight
+echo "Enter your weight"
+read weight
+total=$(( realHeight*weight ))
+echo "The total is $total"
+
 # exercise: ask the user for the width and height and present total
 # number of pixels
