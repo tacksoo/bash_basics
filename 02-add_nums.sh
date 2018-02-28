93834613c6b6844ed4a981b9f0a16046867e19bf
Wil <wnazario@ggc.edu>
2018-02-28 11:32:49 -0500

    modified 02-add_nums.sh to include the exercise commands.

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -7,9 +7,17 @@ read second
 echo "Enter your third number"
 read third
 
+
 sum=$(( first+second+third ))
 
 echo "The sum is $sum"
 
+echo "Enter your width"
+read width
+echo "Enter your height"
+read height
+total=$(( width*height ))
+
+echo "The total is $total"
 # exercise: ask the user for the width and height and present total
 # number of pixels
