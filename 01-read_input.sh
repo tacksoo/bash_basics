502b1432eb69348ecd75491dd3bab0bd5b761308
Elizabeth Haynie <35352425+LizzHaynie@users.noreply.github.com>
2018-03-25 21:46:10 -0400

    Update 01-read_input.sh

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -3,10 +3,12 @@
 
 echo "Hello! What is your name"
 read name
-echo "Welcome, $name"
+echo "Welcome, Elizabeth Haynie"
 
 # single quotes prevent the expansion of the variable
-echo 'Your name was stored in $name'
+echo 'Your name was stored in Elizabeth Haynie'
 
 # exercise: write a script that asks the user for a 
+echo "What is the empty file name?"
 # filename and create an empty file named after it
+read filename
