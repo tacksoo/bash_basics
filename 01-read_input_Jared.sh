0de1cb6224a7be2ded784eaceb22581da4cedda4
jhouseman <jhouseman@ggc.edu>
2018-03-26 21:47:30 -0400

    Edited name prompt

---
+++ 01-read_input_Jared.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+echo "Hello! My name is Jared! What is your name?"
+read name
+echo "Welcome, $name, it is nice to meet you."
+
+# single quotes prevent the expansion of the variable
+echo 'Your name was stored in $name'
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
